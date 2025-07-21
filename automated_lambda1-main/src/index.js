41814525a54dea11476c7b921b3de6bdd1040660
SHUBHAM KUMAR <143086699+SHUBHAMKUMAR900@users.noreply.github.com>
2025-07-21 11:09:47 +0530

    Delete automated_lambda1-main directory

--- automated_lambda1-main/src/index.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "text/plain"
-    },
-    body: "Mera naam Lambda hai! Aap sab kaise hain. CloudTechner me aapka swagat hai.",
-  };
-};
