73875e4f1129e046e5d38d470d35383897cfef5d
SHUBHAM KUMAR <143086699+SHUBHAMKUMAR900@users.noreply.github.com>
2025-07-21 14:31:15 +0530

    Update lambda1.py

--- automated_lambda1-main/lambda1.py
+++ automated_lambda1-main/lambda1.py
@@ -233,25 +233,25 @@ The code will be uploaded to the S3 bucket '{S3_LAMBDA_CODE_BUCKET_TF_REF}' with
 
 Include a source_code_hash (even if placeholder, as GitHub Actions will update it).
 
-For the ALB:
+#For the ALB:
 
-Name: {PROJECT_NAME}-alb
+#Name: {PROJECT_NAME}-alb
 
-Type: application load balancer, internet-facing.
+#Type: application load balancer, internet-facing.
 
-Security group should be the one defined previously.
+#Security group should be the one defined previously.
 
-Subnets: the public subnets defined previously.
+#Subnets: the public subnets defined previously.
 
-Create a target group (lambda_tg) of type lambda that targets the Lambda function.
+#Create a target group (lambda_tg) of type lambda that targets the Lambda function.
 
-Create an HTTP listener on port 80 that forwards traffic to this target group.
+#Create an HTTP listener on port 80 that forwards traffic to this target group.
 
-Grant the ALB permission to invoke the Lambda function using aws_lambda_permission.
+#Grant the ALB permission to invoke the Lambda function using aws_lambda_permission.
 
-Add an alb_dns_name output.
+#Add an alb_dns_name output.
 
-Output the complete, updated main.tf file content in the following format:
+#Output the complete, updated main.tf file content in the following format:
 
 ### main.tf hcl
 ```hcl
